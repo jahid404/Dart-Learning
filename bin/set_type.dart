dc9dd77bbe45037c0254ba45925fec7e601cb841
Jahidul Islam <jsjahidmini@gmail.com>
2026-01-28 21:32:36 +0600

    learned set data type

--- bin/set_type.dart
+++ bin/set_type.dart
@@ -1,3 +1,33 @@
 void main(List<String> args) {
-  
+  var mixedSet = {'dhaka', 'barishal', 'chittagong', 123, true};
+  print(mixedSet);
+
+  var stringSet = <String>{'dhaka', 'barishal', 'chittagong'};
+  print(stringSet);
+
+  var mixedNumSet = <num>{123, 456, 567.89};
+  print(mixedNumSet);
+
+  var intNumSet = <int>{123, 456};
+  print(intNumSet);
+
+  var floatNumSet = <double>{123.45, 456.78};
+  print(floatNumSet);
+
+  var booleanSet = <bool>{true, false};
+  print(booleanSet);
+
+  // adding item to set
+  stringSet.add('noakhali');
+  print(stringSet);
+
+  // hashcode of set
+  print(stringSet.hashCode);
+
+  // access set element
+  print(stringSet.elementAt(1));
+
+  // clearing set element
+  stringSet.clear();
+  print(stringSet);
 }
