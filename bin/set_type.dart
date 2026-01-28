72b2b86c938e314b618b02354ff027367a4fee27
Jahidul Islam <jsjahidmini@gmail.com>
2026-01-28 21:15:43 +0600

    learned object data type

--- bin/set_type.dart
+++ bin/set_type.dart
@@ -1,11 +1,3 @@
 void main(List<String> args) {
-  var userInfo = {'name': 'john', 'age': 25};
-
-  print(userInfo['name']);
-  print(userInfo.length);
-  print(userInfo.keys);
-  print(userInfo.values);
-  print(userInfo.entries);
-  print(userInfo.hashCode);
-  print(userInfo.runtimeType);
+  
 }
