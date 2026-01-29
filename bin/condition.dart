8c4c9411de6388bb22e9321305e0dabf8f1ce173
Jahidul Islam <jsjahidmini@gmail.com>
2026-01-29 22:25:08 +0600

    learned condition

---
+++ bin/condition.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  const number = 124;
+
+  if (number > 100) {
+    print('Number is greater than 100');
+  } else if (number > 120) {
+    print('Number is greater than 120');
+  } else if (number > 150) {
+    print('Number is greater than 150');
+  } else {
+    print('The number is: $number');
+  }
+}
