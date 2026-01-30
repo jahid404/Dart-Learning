f875a0ed1414521bb705c4c934f3677ebf0829e5
Jahidul Islam <jsjahidmini@gmail.com>
2026-01-30 09:24:13 +0600

    learned function

---
+++ bin/function.dart
@@ -0,0 +1,16 @@
+void display(message){
+  print(message);
+}
+
+void main(List<String> args) {
+  int summation(number1, number2) {
+    var result = number1 + number2;
+
+    return result;
+  }
+
+  display(summation(3, 4));
+
+  var res = summation(123, 321);
+  display(res);
+}
