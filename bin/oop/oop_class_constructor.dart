3d908f5998ae5d86d27796a39f8a17be2212b639
Jahidul Islam <jsjahidmini@gmail.com>
2026-01-30 10:10:31 +0600

    feat: Add fields, an `add` method, and update the constructor to use shorthand initialization and call the new method.

--- bin/oop/oop_class_constructor.dart
+++ bin/oop/oop_class_constructor.dart
@@ -1,12 +1,20 @@
 class MyClass {
+  String msg;
+  var num1 = 10;
+  var num2 = 20;
+
+  int add() {
+    return num1 + num2;
+  }
+
   /// 'MyClass' is a constructor function of 'MyClass' class
   /// c.f must be the same name of the class
   /// does not return anything
   /// call automatically
-  MyClass(String msg) {
+  MyClass(this.msg) {
     print('Hey, I am a constructor function!');
-
     print(msg);
+    print(add());
   }
 }
 
